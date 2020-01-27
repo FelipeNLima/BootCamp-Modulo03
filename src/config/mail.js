02e9b60d5a8790ed196fe93726979d31a0f2aed7
Felipe <felipinho.lima27@gmail.com>
2020-01-27 20:31:52 -0300

    subindo

---
+++ src/config/mail.js
@@ -0,0 +1,13 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'c4ce3a64feb014',
+    pass: '4752a90a742366',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  }
+
+};
